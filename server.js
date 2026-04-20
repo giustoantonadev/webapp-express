bb008114086a86a011bfc0bb49bc06b6582667ca
giustoantonadev <giustoantonadev@gmail.com>
2026-04-20 12:33:32 +0200

    install mysql connect to database

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const db = require('./database/db');
+require('dotenv').config();
 
 app.get('/', (req, res) => {
     res.send('Express funziona!');
