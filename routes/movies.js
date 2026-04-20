2ef6e7c01a54ac895774130210940dca3277a4e8
giustoantonadev <giustoantonadev@gmail.com>
2026-04-20 12:50:22 +0200

    add movie and reviews with show

--- routes/movies.js
+++ routes/movies.js
@@ -13,4 +13,29 @@ router.get('/', (req, res) => {
     });
 });
 
+// Show
+router.get('/:id', (req, res) => {
+    const movieId = req.params.id;
+    const sql = 'SELECT * FROM movies WHERE id = ?';
+    const sqlReviews = 'SELECT * FROM reviews WHERE movie_id = ?';
+
+    db.query(sql, [movieId], (err, movieResults) => {
+        if (err) {
+            console.error('Errore nella query del film:', err);
+            return res.status(500).json({ error: 'Errore nel server' });
+        }
+        if (movieResults.length === 0) {
+            return res.status(404).json({ error: 'Film non trovato' });
+        }
+
+        db.query(sqlReviews, [movieId], (err, reviewResults) => {
+            if (err) {
+                console.error('Errore nella query delle recensioni:', err);
+                return res.status(500).json({ error: 'Errore nel server' });
+            }
+            res.json({ movie: movieResults[0], reviews: reviewResults });
+        });
+    });
+});
+
 module.exports = router;
